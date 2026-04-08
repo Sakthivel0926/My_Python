c41e15c8706e62e3cc8b8f20d00fd52a0a77c442
Sakthivel0926 <sakthivel.c2022cse@sece.ac.in>
2026-04-08 12:06:20 +0530

    module

---
+++ main.py
@@ -0,0 +1,10 @@
+import module
+import module1
+
+print("Addition:", module.add(10, 5))
+print("Subtraction:", module.subtract(10, 5))
+print("Multiplication:", module.multiply(10, 5))
+
+print(module1.greet("Sakthivel"))
+print(module1.uppercase("python"))
+print(module1.lowercase("MODULE"))
---
+++ module1.py
@@ -0,0 +1,8 @@
+def greet(name):
+    return f"Hello, {name}!"
+
+def uppercase(text):
+    return text.upper()
+
+def lowercase(text):
+    return text.lower()
